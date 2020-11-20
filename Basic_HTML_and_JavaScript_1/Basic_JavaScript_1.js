e64558406e8ca5ff788fb6bbbc330980bae244ff
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2020-11-20 12:56:26 -0700

    Fix onclick in assignment 2 and other various functionalities

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,6 +1,6 @@
 alert("Your computer will now self destruct!")
 
-function my_first_function () {
+function my_first_function() {
     var String = "Kiss me, I'm (really) Irish!";
     var result = String.fontcolor("green");
     document.getElementById("Irish").innerHTML = result;
