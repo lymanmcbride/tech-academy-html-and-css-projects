436c0e470285539ecdd41c7bcf969839e0b0fd95
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2020-11-20 11:55:33 -0700

    add first method to javascript

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Your computer will now self destruct!")
+
+function my_first_function () {
+    var String = "Kiss me, I'm (really) Irish!";
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML = result;
+}
