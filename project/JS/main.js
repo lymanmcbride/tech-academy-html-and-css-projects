f42c33a35a6f83b6e2e3a7a55fad1df8b98173a1
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-01-04 14:41:25 -0700

    add functionality to lots of things

---
+++ project/JS/main.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("login_form").style.display="block";
+}
+
+function closeForm() {
+    document.getElementById("login_form").style.display="none";
+}
